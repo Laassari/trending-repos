f64e9270b7e94aa67f59d8494f30e6a86b29bea3
laassari <nlaassari@gmail.com>
2020-01-07 02:13:59 +0100

    handle fetch error

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,10 @@
 <template>
   <ul>
-    <Repo v-for="repo in repos" :key="repo.id" :repo="repo" />
-    <RepoPlaceholders v-if="loading" />
+    <template v-if="!fetchError">
+      <Repo v-for="repo in repos" :key="repo.id" :repo="repo" />
+      <RepoPlaceholders v-if="loading" />
+    </template>
+    <div v-else class="error">{{ fetchError }}</div>
   </ul>
 </template>
 
@@ -15,16 +18,26 @@ export default {
     return {
       repos: [],
       loading: false,
+      fetchError: null,
     }
   },
   methods: {
     async fetchPopularGihutbReops() {
       this.loading = true
-      const blob = await fetch('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+      const lastMonthDateInms = Date.now() - 1000 * 60 * 60 * 24 * 30
+      const lastMonthDate = new Date(lastMonthDateInms).toISOString().slice(0, 10)
+
+      const blob = await fetch(
+        `https://api.github.com/search/repositories?q=created:>${lastMonthDate}&sort=stars&order=desc`
+      )
       const response = await blob.json()
 
-      this.loading = false
-      this.repos = response.items
+      if (blob.ok) {
+        this.loading = false
+        this.repos = response.items
+      } else {
+        this.fetchError = 'an error occured. Please try later!'
+      }
     },
   },
   mounted() {
@@ -33,4 +46,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.error {
+  font-size: 2rem;
+  color: #e91e63;
+}
+</style>
